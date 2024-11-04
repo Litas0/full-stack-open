b84a0643c2bda6b0aca6ef35025eb7ad08abfb4f
Litas0 <kubalitke123@gmail.com>
2024-11-04 20:08:33 +0100

    Small changes

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -7,6 +7,8 @@ import Persons from "./components/Persons"
 import Notification from "./components/Notification"
 
 const App = () => {
+  const baseUrl = "/api/persons"
+
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
@@ -15,7 +17,7 @@ const App = () => {
   const [messageGrade, setMessageGrade] = useState('message')
   useEffect(() => {
     axios
-      .get('http://localhost:3001/persons')
+      .get(baseUrl)
       .then(response => {
         setPersons(response.data)
       })
@@ -26,7 +28,7 @@ const App = () => {
     const person = persons.find(person => person.name === newName)
     if (person !== undefined) {
       if (window.confirm(`${person.name} is already added to phonebook, replace the old number with a new one?`)) {
-        const url = `http://localhost:3001/persons/${person.id}`
+        const url = `${baseUrl}/${person.id}`
         const newPerson = {...person, number: newNumber}
         axios
         .put(url, newPerson)
@@ -42,7 +44,7 @@ const App = () => {
       return;
     }
     axios
-     .post('http://localhost:3001/persons', { name: newName, number: newNumber })
+     .post(baseUrl, { name: newName, number: newNumber })
      .then(response => {
         setPersons(persons.concat(response.data))
         setNewName("")
@@ -56,12 +58,12 @@ const App = () => {
   };
 
   const deletePerson = (id) => {
-    const url = `http://localhost:3001/persons/${id}`
+    const url = `${baseUrl}/${id}`
     const person = persons.find(person => person.id === id)
     if (window.confirm('Are you sure you want to delete this person?')) {
       axios.delete(url).then(response => {
         console.log(response)
-        response.status === 200 ? setPersons(persons.filter(person => person.id!== id)) : console.error('Failure deleting')
+        response.status === 204 ? setPersons(persons.filter(person => person.id!== id)) : console.error('Failure deleting')
         setMessageGrade('message')
         setTopMessage(`Successfully deleted ${person.name}`)
         setTimeout(() => {
